2955677bf91a9ecd13a625391765bc4a6e26d20d
sagor <sagor@sagors-MacBook-Pro.local>
2021-06-02 00:11:00 +0600

    add sendGrid mail and redis-pubsub
    Signed-off-by: sagor <sagor@sagors-MacBook-Pro.local>

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/DailyActivity
+
+go 1.16
+
+require (
+	github.com/go-redis/redis/v8 v8.9.0
+	github.com/sendgrid/rest v2.6.4+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.10.0+incompatible
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+)
---
+++ redis-pubsub/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+var ctx context.Context
+
+func main()  {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	pubsub := rdb.Subscribe(ctx, "mychannel1")
+
+	// Wait for confirmation that subscription is created before publishing anything.
+	_, err := pubsub.Receive(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// Go channel which receives messages.
+	ch := pubsub.Channel()
+
+	// Publish a message.
+	err = rdb.Publish(ctx, "mychannel1", "hello").Err()
+	if err != nil {
+		panic(err)
+	}
+
+	time.AfterFunc(time.Second, func() {
+		// When pubsub is closed channel is closed too.
+		_ = pubsub.Close()
+	})
+
+	// Consume messages.
+	for msg := range ch {
+		fmt.Println(msg.Channel, msg.Payload)
+	}
+}
